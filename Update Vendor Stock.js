b2d8799d234af0d89f3fe110df43173d7814638a
Livio Beqiri <liviob@live.com>
2019-04-11 17:03:41 -0400

    add parseInt function to both variables

--- Update Vendor Stock.js
+++ Update Vendor Stock.js
@@ -4,7 +4,7 @@ function ValidateField() {
 
 	var jenne_quantity  = nlapiGetFieldValues('custitem15')
 	var teledynamic_quantity = nlapiGetFieldValues("custitem24")
-    var vendor_stock = jennequantity + teledynamic_quantity 
+    var vendor_stock = parseInt(jennequantity) + parseInt(teledynamic_quantity);  
 	nlapiSetFieldValues('vendorstock', vendor_stock )
 
 

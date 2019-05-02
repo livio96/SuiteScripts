1e341495bafb6692a1213a9f8304d9b5273c4529
Livio Beqiri <liviob@live.com>
2019-05-02 14:51:38 -0400

    Update Logged In Price
    If brand = 'Valcom', set logged in price to jenne cost.

--- updateLoggedInPrice.js
+++ updateLoggedInPrice.js
@@ -1,27 +1,25 @@
-function updatePriceField() {
+function ValidateField() {
+			var jenne_cost = nlapiGetFieldValue('custitem14') ; 
+            var webstore_brand = nlapiGetFieldValue('custitem_awa_brand') ; 
+            nlapiLogExecution('Debug','Category: ',webstore_brand)
+            nlapiLogExecution('Debug','jenne cost: ',jenne_cost)
 
-      var recordType = nlapiGetRecordType(); 
-  		var recordId = nlapiGetRecordId(); 
-  		var item_rec = nlapiLoadRecord(recordType, recordId  ) ; 
-      
-      var min_qty = nlapiGetFieldValue('minimumquantity')
-      item_rec.selectNewLineItem('price1');
-      item_rec.setLineItemValue('price1', 'price_1_', 1, min_qty);
-      item_rec.commitLineItem('price_1_');
-      nlapiSubmitRecord(item_rec) ; 
+             //If jenne cost is not null 
+  			if(jenne_cost !=0 && jenne_cost != null && jenne_cost != undefined) {
+        
+                   //if webstore brand is Valcom 
+        		  if(webstore_brand == 13) {
+        				 var logged_in_price = nlapiGetLineItemValue('price','price_1_',7);
+                    	 var updated_logged_in_price = Math.round(parseFloat(jenne_cost) + parseFloat(0.15 * jenne_cost))
+        			 	 nlapiLogExecution('Debug','Jenne Cost',jenne_cost)
+       	 			 	 nlapiLogExecution('Debug','Logged_in_price',logged_in_price)
+       	 			 	 nlapiLogExecution('Debug','Category: ',webstore_brand)
 
- 
+    	 				 nlapiSelectNewLineItem('price1');
+     	 				 nlapiSetLineItemValue('price', 'price_1_', 7, updated_logged_in_price);
+      					 nlapiCommitLineItem('price_1_');
+         				 nlapiLogExecution('Debug','Logged_in_price',logged_in_price)
 
-function updatePriceField() {
-
-      var recordType = nlapiGetRecordType(); 
-  		var recordId = nlapiGetRecordId(); 
-  		var item_rec = nlapiLoadRecord(recordType, recordId  ) ; 
-
-      var min_qty = nlapiGetFieldValue('minimumquantity')
-      item_rec.selectNewLineItem('price1');
-      item_rec.setLineItemValue('price1', 'price_1_', 1, min_qty);
-      item_rec.commitLineItem('price_1_');
-      nlapiSubmitRecord(item_rec) ;
-
+            			}
+            }
 }

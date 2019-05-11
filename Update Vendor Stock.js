2baef03b02b2e89261b395242d2fc9a4df846903
Livio Beqiri <liviob@live.com>
2019-05-11 10:57:44 -0400

    Update Update Vendor Stock.js

--- Update Vendor Stock.js
+++ Update Vendor Stock.js
@@ -1,12 +1,27 @@
-function ValidateField() {
+function UpdateVendorStockField() {
 
 
 
-	var jenne_quantity  = nlapiGetFieldValues('custitem15')
-	var teledynamic_quantity = nlapiGetFieldValues("custitem24")
-    var vendor_stock = parseInt(jennequantity) + parseInt(teledynamic_quantity);  
-	nlapiSetFieldValues('vendorstock', vendor_stock )
+var jenne_quantity  = nlapiGetFieldValue('custitem15')
+var teledynamic_quantity = nlapiGetFieldValue("custitem24")
+var in_stock = nlapiGetFieldValue('totalquantityonhand')
 
 
+
+
+if(jenne_quantity == null || jenne_quantity== undefined || jenne_quantity =='')
+  		jenne_quantity = parseInt(0)
+if(teledynamic_quantity == null || teledynamic_quantity== undefined || teledynamic_quantity =='') 
+  		teledynamic_quantity = parseInt(0)
+
+ var vendor_stock = parseInt(jenne_quantity) + parseInt(teledynamic_quantity) + parseInt(in_stock);
+  
+        nlapiLogExecution('Debug','Jenne: ',  jenne_quantity)
+          nlapiLogExecution('Debug','tele : ',  teledynamic_quantity)
+            nlapiLogExecution('Debug','stock : ',  in_stock)
+            nlapiLogExecution('Debug','vendor stock : ',  vendor_stock)
+  
+	    nlapiSetFieldValue('custitem_vendor_stock', parseInt(vendor_stock) )
 
 }
+

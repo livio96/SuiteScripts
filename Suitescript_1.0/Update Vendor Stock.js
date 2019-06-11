20617b8b2203982ce144d5719894dde83228bd83
Livio Beqiri <liviob@live.com>
2019-06-11 15:37:12 -0400

    Do a saved search to get defective qty

--- Suitescript_1.0/Update Vendor Stock.js
+++ Suitescript_1.0/Update Vendor Stock.js
@@ -10,8 +10,8 @@ if(subsidiary !=null && itemInternalId !=null) {
 var itemSearch = nlapiSearchRecord("item",null,
 			[
 			["internalid","anyof",itemInternalId],
-			//"AND",
-			//["subsidiary","anyof",subsidiary]
+			"AND",
+			["subsidiary","anyof",subsidiary]
 			],
 			[
 		
@@ -26,8 +26,17 @@ var itemSearch = nlapiSearchRecord("item",null,
 //If number of results > 1 
 if(itemSearch) {
 
-  var defective = itemSearch[7].getValue('locationquantityonhand') ; 
-
+  for(var i=0 ; i<itemSearch.length ; i++)
+		{
+			var colsAll = itemSearch[i].getAllColumns();
+			var location_quantity = itemSearch[i].getValue(colsAll[0]);
+          	var location_name = itemSearch[i].getValue(colsAll[1]);
+            if(location_name == "Defective") {
+              var loc_name = location_name; 
+               var defective_quantity = location_quantity; 
+            }
+		}
+  
 var jenne_quantity  = nlapiGetFieldValue('custitem15')
 var teledynamic_quantity = nlapiGetFieldValue("custitem24")
 var in_stock = nlapiGetFieldValue('totalquantityonhand')
@@ -43,15 +52,11 @@ if(jenne_quantity == null || jenne_quantity== undefined || jenne_quantity =='')
 if(teledynamic_quantity == null || teledynamic_quantity== undefined || teledynamic_quantity =='') 
   		teledynamic_quantity = parseInt(0)
 
- var vendor_stock = parseInt(jenne_quantity) + parseInt(teledynamic_quantity) + parseInt(in_stock) - defective;
+ var vendor_stock = parseInt(jenne_quantity) + parseInt(teledynamic_quantity) + parseInt(in_stock) - defective_quantity;
   
-        nlapiLogExecution('Debug','Jenne: ',  jenne_quantity)
-          nlapiLogExecution('Debug','tele : ',  teledynamic_quantity)
-            nlapiLogExecution('Debug','stock : ',  in_stock)
-            nlapiLogExecution('Debug','vendor stock : ',  vendor_stock)
-    			nlapiLogExecution('Debug', 'defective ', defective) ; 
-
-    			nlapiLogExecution('Debug', 'location ', location) ; 
+        
+    			nlapiLogExecution('Debug', 'name  ', loc_name) ; 
+    			nlapiLogExecution('Debug', 'defective quantity ', defective_quantity) ; 
 
 
 

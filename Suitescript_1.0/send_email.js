92c513ff1e382f4b4b214fd4895a4ba8e88f3d24
Livio Beqiri <liviob@live.com>
2019-07-29 17:56:31 -0400

    Uploaded 'send an email' script
    This scripts sends an automatic email to a customer.

---
+++ Suitescript_1.0/send_email.js
@@ -0,0 +1,11 @@
+function send_email(){
+  
+
+// Send an email to xyz@gmail.com
+nlapiSendEmail(1, 'xyz@gmail.com', 
+               'Invoice Receipt', 'your order has been completed', 
+               null, null, null, null, true, null, 'johnsmith@gmail.com');
+
+
+
+}

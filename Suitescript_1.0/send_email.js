563df45fa32e79b3cbd708ff3d4cd070debcc4c0
Livio Beqiri <liviob@live.com>
2019-07-29 21:24:48 -0400

    Attach an email template

--- Suitescript_1.0/send_email.js
+++ Suitescript_1.0/send_email.js
@@ -6,6 +6,18 @@ nlapiSendEmail(1, 'xyz@gmail.com',
                'Invoice Receipt', 'your order has been completed', 
                null, null, null, null, true, null, 'johnsmith@gmail.com');
 
+  
+//Attach an email template
+/*function send_email(){
+ var emailMerger = nlapiCreateEmailMerger(1458);
 
+var mergeResult = emailMerger.merge();
+var body = mergeResult.getBody();
+var subject = mergeResult.getSubject();
+
+nlapiSendEmail(nlapiGetUser(), 'lbeqiri@telquestintl.com', subject, body, null, null, null, null);
+
+}
+*/
 
 }

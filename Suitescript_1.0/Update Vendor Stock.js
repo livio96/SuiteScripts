684af05e69f8545ced3b2f9a63d0cd8e03cf3f74
Livio Beqiri <liviob@live.com>
2020-04-10 14:47:40 -0400

    Update Update Vendor Stock.js

--- Suitescript_1.0/Update Vendor Stock.js
+++ Suitescript_1.0/Update Vendor Stock.js
@@ -1,69 +1,87 @@
 /* Suitescript 1.0 
  * Update Vendor Stock (custom field)
- * Vendor Stock field is set to quantityavailable + jenne_quantity (custom_field)
  * Created by Livio Beqiri
  */
-
 function stock_calculation() {
 
-    var item_id = nlapiGetRecordId();
-    var context = nlapiGetContext().getExecutionContext();
-	var web_default_availability = nlapiGetFieldValue('custitem_webstore_default_availibility');
-	var userId = nlapiGetUser();
+    var userId = nlapiGetUser();
+
+    var item_id = nlapiGetFieldValue('itemid');
+    var internal_id = nlapiGetFieldValue('internalid');
+    nlapiLogExecution('Debug', 'item', item_id)
+ 	
  
-   if (item_id && userId === 1692630) {
+  
+  //  if (item_id && userId === 1692630) {
+
+
         var itemSearch = nlapiSearchRecord("item", null,
             [
-                ["internalid", "anyof", item_id]
+                ["name", "is", item_id],
+                "AND",
+                ["inventorylocation", "anyof", "1", "27"],
+                "AND",
+                ["isinactive", "is", "F"]
             ],
             [
-                new nlobjSearchColumn("quantityavailable").setSort(true)
-
-
-
+                new nlobjSearchColumn("inventorylocation"),
+                new nlobjSearchColumn("locationquantityavailable")
             ]
         );
-        if (itemSearch && item_id) {
-            var quantity_available = itemSearch[0].getValue('quantityavailable');
-        }
+        if (itemSearch) {
+            var quantity_available_wh_A = itemSearch[0].getValue('locationquantityavailable');
+            //nlapiLogExecution('Debug', 'wh_a', quantity_available_wh_A )
+
+            if (quantity_available_wh_A == null || quantity_available_wh_A == '') {
+                quantity_available_wh_A = parseFloat(0);
+            }
+
+
+            var quantity_available_wh_24P = itemSearch[1].getValue('locationquantityavailable');
+            //nlapiLogExecution('Debug', '24p',quantity_available_wh_24P )
+            if (quantity_available_wh_24P == null || quantity_available_wh_24P == '') {
+                quantity_available_wh_24P = parseFloat(0);
+            }
+            var quantity_available = parseFloat(quantity_available_wh_A) + parseFloat(quantity_available_wh_24P)
+            if (quantity_available == null || quantity_available == '') {
+                quantity_available = parseFloat(0);
+            }
+
+            var customrecord_awa_vendor_info_itemsSearch = nlapiSearchRecord("customrecord_awa_vendor_info_items", null,
+                [
+                    ["custrecord_vifi_item", "anyof", internal_id]
+                ],
+                [
+                    new nlobjSearchColumn("custrecord_vifi_quantity")
+
+                ]
+            );
+
+
+            var qty = 0;
+            var vendor_stock = 0
+
+
+            if (customrecord_awa_vendor_info_itemsSearch) {
+                for (i = 0; i < customrecord_awa_vendor_info_itemsSearch.length; i++) {
+                    if (customrecord_awa_vendor_info_itemsSearch[i].getValue('custrecord_vifi_quantity') != null && customrecord_awa_vendor_info_itemsSearch[i].getValue('custrecord_vifi_quantity') != '')
+                        qty += parseFloat(customrecord_awa_vendor_info_itemsSearch[i].getValue('custrecord_vifi_quantity'));
+
+                }
+            }
+            if (quantity_available == null || quantity_available == '')
+                quantity_available = parseFloat(0);
+            if (qty == null || qty == '')
+                qty = parseFloat(0)
 
 
 
-        var jenne_quantity = nlapiGetFieldValue('custitem15');
+            vendor_stock = parseFloat(qty) + parseFloat(quantity_available);
+  			vendor_stock = Math.round(vendor_stock);
+            nlapiLogExecution('Debug', 'final ', vendor_stock);
 
-        if (jenne_quantity == null || jenne_quantity == '')
-            jenne_quantity = parseInt(0)
+            nlapiSetFieldValue('custitem_vendor_stock', vendor_stock);
 
-        if (quantity_available == null || quantity_available == '') {
-            quantity_available = parseInt(0);
-        }
-
-        var vendor_stock = parseInt(jenne_quantity) + parseInt(quantity_available);
-
-        nlapiLogExecution('Debug', 'vendor stock : ', vendor_stock)
-        nlapiSetFieldValue('custitem_vendor_stock', parseInt(vendor_stock));
-
-      
-        if(vendor_stock > 0 && (web_default_availability === '' || web_default_availability === null)){
-              nlapiSetFieldValue('custitem_item_availibility', '1'); 
-        }
-  		else if(web_default_availability === '1'){
-          nlapiSetFieldValue('custitem_item_availibility', '1'); 
-          
-        }
-     	else if(web_default_availability === '3'){
-          nlapiSetFieldValue('custitem_item_availibility', '3');
-        }
-  		else if(web_default_availability === '2'){
-           nlapiSetFieldValue('custitem_item_availibility', '2'); 
-        }
-  		else{
-            nlapiSetFieldValue('custitem_item_availibility', '2'); 
-        }
-
-
+        //}
     }
-
-
-
 }

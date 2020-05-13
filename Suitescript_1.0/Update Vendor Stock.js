3c63401f438d0b92849c6ef9dfb230266574e198
Livio Beqiri <liviob@live.com>
2020-05-13 07:55:34 -0400

    three saved searches

--- Suitescript_1.0/Update Vendor Stock.js
+++ Suitescript_1.0/Update Vendor Stock.js
@@ -1,87 +1,114 @@
-/* Suitescript 1.0 
- * Update Vendor Stock (custom field)
+/*Calculate Vendor Stock (custom field) 
  * Created by Livio Beqiri
- */
-function stock_calculation() {
+ * 05/19/2019
+*/
 
-    var userId = nlapiGetUser();
+function stock_calculation(){
 
-    var item_id = nlapiGetFieldValue('itemid');
-    var internal_id = nlapiGetFieldValue('internalid');
-    nlapiLogExecution('Debug', 'item', item_id)
- 	
- 
-  
-  //  if (item_id && userId === 1692630) {
+    nlapiLogExecution('Debug', 'Success', 'Success')
+    var item_id = nlapiGetFieldValue('internalid');
+    var item_name = nlapiGetFieldValue('itemid');
+    var smart_webstore_qty = parseFloat(0);
+    var vendor_quantity = 0 ;
+    var telquest_quantity_24P = 0;
+    var telquest_quantity_A = 0;
 
+    //Get Smart Vendor Quantity
+    var customrecord_swSearch = nlapiSearchRecord("customrecord_sw",null,
+        [
+            ["custrecord_sw_item","anyof",item_id],
+            "AND",
+            ["custrecord_sw_webstore","anyof","1"]
+        ],
+        [
+            new nlobjSearchColumn("custrecord_sw_quantity_available",null,"SUM"),
+       		new nlobjSearchColumn("custrecord_sw_item",null,"GROUP")
+        ]
+    );
 
-        var itemSearch = nlapiSearchRecord("item", null,
-            [
-                ["name", "is", item_id],
-                "AND",
-                ["inventorylocation", "anyof", "1", "27"],
-                "AND",
-                ["isinactive", "is", "F"]
-            ],
-            [
-                new nlobjSearchColumn("inventorylocation"),
-                new nlobjSearchColumn("locationquantityavailable")
-            ]
-        );
-        if (itemSearch) {
-            var quantity_available_wh_A = itemSearch[0].getValue('locationquantityavailable');
-            //nlapiLogExecution('Debug', 'wh_a', quantity_available_wh_A )
-
-            if (quantity_available_wh_A == null || quantity_available_wh_A == '') {
-                quantity_available_wh_A = parseFloat(0);
-            }
-
-
-            var quantity_available_wh_24P = itemSearch[1].getValue('locationquantityavailable');
-            //nlapiLogExecution('Debug', '24p',quantity_available_wh_24P )
-            if (quantity_available_wh_24P == null || quantity_available_wh_24P == '') {
-                quantity_available_wh_24P = parseFloat(0);
-            }
-            var quantity_available = parseFloat(quantity_available_wh_A) + parseFloat(quantity_available_wh_24P)
-            if (quantity_available == null || quantity_available == '') {
-                quantity_available = parseFloat(0);
-            }
-
-            var customrecord_awa_vendor_info_itemsSearch = nlapiSearchRecord("customrecord_awa_vendor_info_items", null,
-                [
-                    ["custrecord_vifi_item", "anyof", internal_id]
-                ],
-                [
-                    new nlobjSearchColumn("custrecord_vifi_quantity")
-
-                ]
-            );
-
-
-            var qty = 0;
-            var vendor_stock = 0
-
-
-            if (customrecord_awa_vendor_info_itemsSearch) {
-                for (i = 0; i < customrecord_awa_vendor_info_itemsSearch.length; i++) {
-                    if (customrecord_awa_vendor_info_itemsSearch[i].getValue('custrecord_vifi_quantity') != null && customrecord_awa_vendor_info_itemsSearch[i].getValue('custrecord_vifi_quantity') != '')
-                        qty += parseFloat(customrecord_awa_vendor_info_itemsSearch[i].getValue('custrecord_vifi_quantity'));
-
-                }
-            }
-            if (quantity_available == null || quantity_available == '')
-                quantity_available = parseFloat(0);
-            if (qty == null || qty == '')
-                qty = parseFloat(0)
-
-
-
-            vendor_stock = parseFloat(qty) + parseFloat(quantity_available);
-  			vendor_stock = Math.round(vendor_stock);
-            nlapiLogExecution('Debug', 'final ', vendor_stock);
-
-            nlapiSetFieldValue('custitem_vendor_stock', vendor_stock);
-
-        //}
+    if (customrecord_swSearch) {
+        smart_webstore_qty = parseFloat(customrecord_swSearch[0].getValue("custrecord_sw_quantity_available",null,"SUM"));
+        if(smart_webstore_qty === null || smart_webstore_qty === ''){
+          smart_webstore_qty  = parseFloat(0);
+        } 
     }
+
+
+    //Get Vendor Quantity
+    var customrecord_awa_vendor_info_itemsSearch = nlapiSearchRecord("customrecord_awa_vendor_info_items",null,
+        [
+            ["isinactive","is","F"],
+            "AND",
+            ["custrecord_vifi_item","anyof",item_id]
+        ],
+        [
+            new nlobjSearchColumn("custrecord_vifi_quantity",null,"SUM")
+        ]
+    );
+
+     if(customrecord_awa_vendor_info_itemsSearch){
+         vendor_quantity = customrecord_awa_vendor_info_itemsSearch[0].getValue("custrecord_vifi_quantity",null,"SUM");
+         if(vendor_quantity === null || vendor_quantity === '')
+          vendor_quantity  = parseFloat(0);
+     }
+
+     //Get Telquest Quantity
+    var inventoryitemSearch = nlapiSearchRecord("inventoryitem",null,
+        [
+            ["isinactive","is","F"],
+            "AND",
+            ["name","doesnotcontain","retired"],
+            "AND",
+            ["type","anyof","InvtPart"],
+            "AND",
+            ["inventorylocation","anyof","27"],
+            "AND",
+            ["name","contains",item_name]
+            
+        ],
+        [
+            new nlobjSearchColumn("locationquantityavailable",null,"GROUP")
+        ]
+    );
+     
+     if(inventoryitemSearch){
+         telquest_quantity_24P = inventoryitemSearch[0].getValue("locationquantityavailable",null,"GROUP");
+          if(telquest_quantity_24P === null || telquest_quantity_24P === '')
+          telquest_quantity_24P  = parseFloat(0);
+     }
+     
+  
+    var inventoryitemSearch = nlapiSearchRecord("inventoryitem",null,
+        [
+            ["isinactive","is","F"],
+            "AND",
+            ["name","doesnotcontain","retired"],
+            "AND",
+            ["type","anyof","InvtPart"],
+            "AND",
+            ["inventorylocation","anyof","1"],
+            "AND",
+            ["name","contains",item_name]
+        ],
+        [
+            new nlobjSearchColumn("locationquantityavailable",null,"GROUP")
+        ]
+    );
+     
+     if(inventoryitemSearch){
+         telquest_quantity_A = inventoryitemSearch[0].getValue("locationquantityavailable",null,"GROUP");
+        if(telquest_quantity_A === null || telquest_quantity_A === '')
+          telquest_quantity_A  = parseFloat(0);
+     }
+     
+      
+         nlapiLogExecution('Debug', 'vendor_qty', vendor_quantity)
+    nlapiLogExecution('Debug', 'smart web', smart_webstore_qty)
+    nlapiLogExecution('Debug', 'A',telquest_quantity_A )
+    nlapiLogExecution('Debug', '24p', telquest_quantity_24P)
+
+     nlapiSetFieldValue('custitem_vendor_stock', parseFloat(telquest_quantity_24P) +parseFloat(smart_webstore_qty)+parseFloat(vendor_quantity) + parseFloat(telquest_quantity_A));
+    
 }
+
+

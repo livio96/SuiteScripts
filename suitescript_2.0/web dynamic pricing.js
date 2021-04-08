7176978beca2c3c417f3d2e3ce7bfba5dd70cdd2
Livio Beqiri <liviob@live.com>
2021-04-08 17:09:30 -0400

    Update web dynamic pricing.js

--- suitescript_2.0/web dynamic pricing.js
+++ suitescript_2.0/web dynamic pricing.js
@@ -2,57 +2,267 @@
  *@NApiVersion 2.x
  *@NScriptType UserEventScript
  */
-define(['N/record', 'N/runtime'],
+define(['N/search', 'N/record', 'N/runtime', 'N/file', 'N/format', 'N/log'],
 
-    function(record, runtime) {
-
+    function(search, record, runtime, file, format, log) {
 
         function beforeSubmit(context) {
 
-            var newRecord = context.newRecord;
+            try {
+                var newRecord = context.newRecord;
 
-            var user = runtime.getCurrentUser();
-            var user_id = user.id
-            //if user is Livio
-            if (user_id == '1692630') {
 
-                var vendor_stock = newRecord.getValue({
-                    fieldId: 'custitem_vendor_stock',
+                var item_id = newRecord.getValue({
+                    fieldId: 'itemid'
                 });
-
-          
-                var jenne_cost = newRecord.getValue({
-                    fieldId: 'custitem14',
-                });
-
-                var jenne_qty = newRecord.getValue({
-                    fieldId: 'custitem15',
+                var webstore_pricing_rule = newRecord.getValue({
+                    fieldId: 'custitem_website_pricing_rules'
                 });
 
 
-                var sublistFieldValue = newRecord.getSublistValue({
-                    sublistId: 'price1',
-                    fieldId: 'price_1_',
-                    line: 10
-                });
+  
 
-
-            
-                if (vendor_stock == jenne_qty) {
-
-                    newRecord.setSublistValue({
-                        sublistId: 'price1',
-                        fieldId: 'price_1_',
-                        line: 10,
-                        value: parseFloat(jenne_cost) + parseFloat(jenne_cost * 0.15)
+                //if Match Amazon Price
+                if (webstore_pricing_rule === '2') {
+                    var itemSearchObj = search.create({
+                        type: "item",
+                        filters: [
+                            ["isinactive", "is", "F"],
+                            "AND",
+                            ["name", "contains", item_id]
+                        ],
+                        columns: [
+                            search.createColumn({
+                                name: "itemid",
+                                summary: "GROUP",
+                                label: "Name"
+                            }),
+                            search.createColumn({
+                                name: "custrecordamazon_price_30_days",
+                                join: "CUSTRECORD17",
+                                summary: "GROUP",
+                                label: "Amazon Price (Average 30 Days)"
+                            }),
+                            search.createColumn({
+                                name: "custrecord19",
+                                join: "CUSTRECORD17",
+                                summary: "GROUP",
+                                label: "Amazon Price (Average 7 Days)"
+                            }),
+                            search.createColumn({
+                                name: "custrecord_so_rate_30_days",
+                                join: "CUSTRECORD17",
+                                summary: "GROUP",
+                                label: "SO Rate (30 Days)"
+                            }),
+                            search.createColumn({
+                                name: "custrecordso_rate_7_days",
+                                join: "CUSTRECORD17",
+                                summary: "GROUP",
+                                label: "SO Rate (7 Days)"
+                            })
+                        ]
                     });
+
+                    var searchResults = itemSearchObj.run();
+                    var searchResultsRange = searchResults.getRange(0, 1000);
+
+                    var amazon_7_days = searchResultsRange[0].getValue({
+                        name: "custrecord19",
+                        join: "CUSTRECORD17",
+                        summary: "GROUP"
+                    });
+                    var amazon_30_days = searchResultsRange[0].getValue({
+                        name: "custrecordamazon_price_30_days",
+                        join: "CUSTRECORD17",
+                        summary: "GROUP"
+                    });
+
+                    amazon_7_days = format.parse({
+                        value: amazon_7_days,
+                        type: format.Type.INTEGER
+                    });
+                    amazon_30_days = format.parse({
+                        value: amazon_30_days,
+                        type: format.Type.INTEGER
+                    });
+
+                    var set_price = 'false';
+                    if (amazon_7_days > 0) {
+                        set_price = 'true';
+                        newRecord.setSublistValue({
+                            sublistId: 'price1',
+                            fieldId: 'price_1_',
+                            line: 2,
+                            value: amazon_7_days - 0.01
+                        });
+                    }
+                    if (set_price === 'false' && amazon_30_days > 0) {
+
+                        newRecord.setSublistValue({
+                            sublistId: 'price1',
+                            fieldId: 'price_1_',
+                            line: 2,
+                            value: amazon_30_days - 0.01
+                        });
+                    }
+
+
+
+                }
+                // if match ebay price
+                if (webstore_pricing_rule === '3') {
+                    var itemSearchObj = search.create({
+                        type: "item",
+                        filters: [
+                            ["isinactive", "is", "F"],
+                            "AND",
+                            ["name", "contains", item_id]
+                        ],
+                        columns: [
+                            search.createColumn({
+                                name: "itemid",
+                                summary: "GROUP",
+                                label: "Name"
+                            }),
+                            search.createColumn({
+                                name: "custrecord_ebay_price_7_days",
+                                join: "CUSTRECORD17",
+                                summary: "GROUP",
+                                label: "Ebay Price (Average 7 Days)"
+                            }),
+                            search.createColumn({
+                                name: "custrecordebay_price_30_days",
+                                join: "CUSTRECORD17",
+                                summary: "GROUP",
+                                label: "Ebay Price (Average 30 Days)"
+                            })
+
+
+                        ]
+                    });
+
+                    var searchResults3 = itemSearchObj.run();
+                    var searchResultsRange3 = searchResults3.getRange(0, 1000);
+
+                    var ebay_7_days = searchResultsRange3[0].getValue({
+                        name: "custrecord_ebay_price_7_days",
+                        join: "CUSTRECORD17",
+                        summary: "GROUP"
+                    });
+                    var ebay_30_days = searchResultsRange3[0].getValue({
+                        name: "custrecordebay_price_30_days",
+                        join: "CUSTRECORD17",
+                        summary: "GROUP"
+                    });
+
+                    ebay_7_days = format.parse({
+                        value: ebay_7_days,
+                        type: format.Type.INTEGER
+                    });
+                    ebay_30_days = format.parse({
+                        value: ebay_30_days,
+                        type: format.Type.INTEGER
+                    });
+
+                    var set_price = 'false';
+                    if (ebay_7_days > 0) {
+                        set_price = 'true';
+                        newRecord.setSublistValue({
+                            sublistId: 'price1',
+                            fieldId: 'price_1_',
+                            line: 2,
+                            value: ebay_7_days - 0.01
+                        });
+                    }
+                    if (set_price === 'false' && ebay_30_days > 0) {
+
+                        newRecord.setSublistValue({
+                            sublistId: 'price1',
+                            fieldId: 'price_1_',
+                            line: 2,
+                            value: ebay_30_days - 0.01
+                        });
+                    }
+
+
+                }
+                //if Match TQ SO Rate 
+                if (webstore_pricing_rule === '4') {
+                    var itemSearchObj = search.create({
+                        type: "item",
+                        filters: [
+                            ["isinactive", "is", "F"],
+                            "AND",
+                            ["name", "contains", item_id]
+                        ],
+                        columns: [
+                            search.createColumn({
+                                name: "itemid",
+                                summary: "GROUP",
+                                label: "Name"
+                            }),
+                            search.createColumn({
+                                name: "custrecord_so_rate_30_days",
+                                join: "CUSTRECORD17",
+                                summary: "GROUP",
+                                label: "SO Rate (30 Days)"
+                            }),
+                            search.createColumn({
+                                name: "custrecordso_rate_7_days",
+                                join: "CUSTRECORD17",
+                                summary: "GROUP",
+                                label: "SO Rate (7 Days)"
+                            })
+                        ]
+                    });
+
+                    var searchResults2 = itemSearchObj.run();
+                    var searchResultsRange2 = searchResults2.getRange(0, 1000);
+
+                    var so_rate_7_days = searchResultsRange2[0].getValue({
+                        name: "custrecordso_rate_7_days",
+                        join: "CUSTRECORD17",
+                        summary: "GROUP"
+                    });
+                    var so_rate_30_days = searchResultsRange2[0].getValue({
+                        name: "custrecord_so_rate_30_days",
+                        join: "CUSTRECORD17",
+                        summary: "GROUP"
+                    });
+                    var set_price = 'false';
+                    if (so_rate_7_days > 0) {
+                        set_price = 'true';
+                        newRecord.setSublistValue({
+                            sublistId: 'price1',
+                            fieldId: 'price_1_',
+                            line: 2,
+                            value: so_rate_7_days - 0.01
+                        });
+                    }
+                    if (set_price === 'false' && so_rate_30_days > 0) {
+                        newRecord.setSublistValue({
+                            sublistId: 'price1',
+                            fieldId: 'price_1_',
+                            line: 2,
+                            value: so_rate_30_days - 0.01
+                        });
+                    }
 
 
                 }
 
+
+
+            } catch (e) {
+                log.debug({
+                    title: 'Error',
+                    details: 'Error'
+                });
             }
 
         }
+
 
         return {
             beforeSubmit: beforeSubmit,

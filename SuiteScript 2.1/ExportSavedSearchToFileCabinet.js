6de821a2213a270f904763a751174ffe83102ad1
Livio Beqiri <liviob@live.com>
2023-10-09 09:41:36 -0400

    Export 2 searches at the same time

--- SuiteScript 2.1/ExportSavedSearchToFileCabinet.js
+++ SuiteScript 2.1/ExportSavedSearchToFileCabinet.js
@@ -1,37 +1,46 @@
 /**
- *@NApiVersion 2.1
- *@NScriptType ScheduledScript
+*@NApiVersion 2.1
+*@NScriptType ScheduledScript
  */
 define(['N/search', 'N/record', 'N/log', 'N/task'], (search, record, log, task) => {
     let execute = (c) => {
 
-        //Saved search ID
-        const savedSearchId = 379596;
-
+    //________Brokerbin Inventory List
+    //Brokerbin Inventory with First Choice Price [Do not Delete]
+     const savedSearchId = 379596;
         var myTask = task.create({
             taskType: task.TaskType.SEARCH
         });
-
         myTask.savedSearchId = savedSearchId;
-
-
         myTask.fileId = 17576240;
-
-
         var myTaskId = myTask.submit();
-
         var taskStatus = task.checkStatus({
             taskId: myTaskId
         });
 
-        if (taskStatus.status === task.TaskStatus.COMPLETE) {
-            log.debug('export completed', 'export completed');
-        }
+    //____________Business Price Inventory List_____________________
+   //Brokerbin Inventory with Business Price [Do not Delete]
+     savedSearchId = 381057;
+        var myTask = task.create({
+            taskType: task.TaskType.SEARCH
+        });
+        myTask.savedSearchId = savedSearchId;
+        myTask.fileId = 19703332;
+         myTaskId = myTask.submit();
+         taskStatus = task.checkStatus({
+            taskId: myTaskId
+        });
 
+    // Optionally, add logic that executes when the task is complete
+    if (taskStatus.status === task.TaskStatus.COMPLETE) {
+        log.debug('export completed', 'export completed'); 
+    }
+      
     }
 
 
     return {
         execute: execute
     };
-});
+}
+);

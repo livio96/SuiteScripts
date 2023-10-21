d12591b83dff0adeb92f6357f6f7cc1cf3c8cf63
Livio Beqiri <liviob@live.com>
2023-10-21 09:13:41 -0400

    Update Sample_suitelet.js

--- Suitelets/Sample_suitelet.js
+++ Suitelets/Sample_suitelet.js
@@ -1,8 +1,18 @@
-function suitelet(request, response){
-	
-	var html = '<html><body><h1>Hello World</h1></body></html>';
+/**
+ * @NApiVersion 2.x
+ * @NScriptType Suitelet
+ */
+define(['N/record', 'N/search', 'N/ui/serverWidget'], function(record, search, serverWidget) {
+    function onRequest(context) {
+        var html = '<html><body><h1>Hello World</h1></body></html>';
+        context.response.write(html);
+        context.response.setHeader({
+            name: 'Custom-Header-Demo',
+            value: 'Demo'
+        });
+    }
 
-	response.write(html); 
-	response.setHeader('custom-header-demo', 'demo');
-
-}
+    return {
+        onRequest: onRequest
+    };
+});
